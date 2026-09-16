f5f0b550e7b4e227a8f129a7ccccefd4482ab70b
Jithu-2005 <jithendrababuyandluri30@gmail.com>
2026-09-16 15:19:43 +0530

    Changes

--- business_logic/models.py
+++ business_logic/models.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 
 from dataaccess.data_models import Booking, Movie
 
-class Movie(BaseModel):
+class MovieCreate(BaseModel):
     Movie_name: str
     Theatre_name: str
     Ticket_price: int
